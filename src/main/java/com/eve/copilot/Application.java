eef4dba9d2f2d846c4173594bd64a867bd2d7dc9
Afalinko <afalin@list.ru>
2017-09-09 23:58:34 -0400

    Replaces system type from sig ID

--- src/main/java/com/eve/copilot/Application.java
+++ src/main/java/com/eve/copilot/Application.java
@@ -1,27 +1,61 @@
 package com.eve.copilot;
 
+import com.google.gson.Gson;
+
 import java.awt.*;
 import java.awt.datatransfer.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Application {
 
-    private static String bookmarkFromScanLine(String scanLine){
+    private static String bookmarkFromScanLine(String scanLine) {
         if (!scanLine.matches("[A-Z]{3}-[0-9]{3}\t[A-Za-z ]+\t[A-Za-z ]+\t[A-Za-z -0-9]+\t100.0%\t[0-9\\.,]+ [A-Za-z]+$")) {
             return null;
         }
 
         String[] data_parts = scanLine.split("\t");
         char type = data_parts[2].charAt(0);
-        if (type=='W'){
+        if (type == 'W') {
             return data_parts[0].substring(0, 3) + "-C?-K162";
         } else {
             return data_parts[0].substring(0, 3) + "-" + data_parts[2].charAt(0) + "-" + data_parts[3];
         }
     }
 
+    private static Map<String, String> loadSigs() {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("wh_sig.json");
+
+        Gson g = new Gson();
+        Signature[] sigDb = g.fromJson(new BufferedReader(new InputStreamReader(is)), Signature[].class);
+        System.out.println("Signatures loaded: " + sigDb.length);
+        return Arrays.stream(sigDb).collect(Collectors.toMap(o -> o.sig, o -> o.val));
+    }
+
+    private static String expandSig(String scanLine, Map<String, String> sigDB) {
+        if (!scanLine.matches("[A-Z]{3}-C\\?-[A-Za-z][0-9]+")) {
+            return null;
+        }
+
+        final String[] data_parts = scanLine.split("-");
+        if (sigDB.containsKey(data_parts[2])) {
+            final String val = sigDB.get(data_parts[2]);
+            return data_parts[0] + "-" + val + "-" + data_parts[2];
+        }
+
+        return null;
+    }
+
     public static void main(String args[]) throws InterruptedException {
         String data = "";
+        Map<String, String> sigDB = loadSigs();
         while (true) {
             String clipboardContents = getClipboardContents();
             if (!data.equalsIgnoreCase(clipboardContents)) {
@@ -29,10 +63,16 @@ public class Application {
 
                 if (!data.contains("\n")) {
                     final String bookmark = bookmarkFromScanLine(data);
-                    if (bookmark!=null){
+                    if (bookmark != null) {
                         StringSelection selection = new StringSelection(bookmark);
                         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, selection);
-                        System.out.println("set: "+bookmark);
+                        System.out.println("set: " + bookmark);
+                    }
+                    final String sigExpansion = expandSig(data, sigDB);
+                    if (sigExpansion != null) {
+                        StringSelection selection = new StringSelection(sigExpansion);
+                        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, selection);
+                        System.out.println("set: " + sigExpansion);
                     }
                 }
 
@@ -41,7 +81,7 @@ public class Application {
         }
     }
 
-    public static String getClipboardContents() {
+    private static String getClipboardContents() {
         String result = "";
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 
@@ -52,10 +92,7 @@ public class Application {
         if (hasTransferableText) {
             try {
                 result = (String) contents.getTransferData(DataFlavor.stringFlavor);
-            } catch (UnsupportedFlavorException ex) {
-                System.out.println(ex);
-                ex.printStackTrace();
-            } catch (IOException ex) {
+            } catch (UnsupportedFlavorException | IOException ex) {
                 System.out.println(ex);
                 ex.printStackTrace();
             }
---
+++ src/main/java/com/eve/copilot/Signature.java
@@ -0,0 +1,28 @@
+package com.eve.copilot;
+
+public class Signature {
+
+    public String sig;
+    public String val;
+
+    public Signature(String sig, String val) {
+        this.sig = sig;
+        this.val = val;
+    }
+
+    public String getSig() {
+        return sig;
+    }
+
+    public void setSig(String sig) {
+        this.sig = sig;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+}
